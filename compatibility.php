76db0e8034378f9cbf104917c2511e2a09e1229e
Yvonne Chen <evechen@uw.edu>
2015-12-01 16:54:22 -0800

    HTML-ified

--- compatibility.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo '<div id="compatibility">
-				Tested on Chrome 30+, IE ?, Firefox ?
-	</div>';
-?>
--- footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo '<div class="dividertop"></div>
-		<div id="footer">
-			<div class="left">&copy; 2015 Yvonne Chen</div>
-			<div class="right">Written in Notepad++. Developed for Chrome.</div>
-	</div>';
-?>
